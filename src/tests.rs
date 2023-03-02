cd7ecde1c9435367e25e98648568a6a4599b2c72
obvMellow <darkenedmellow@protonmail.com>
2023-03-02 16:35:35 +0300

    Fixed key in test

--- src/tests.rs
+++ src/tests.rs
@@ -5,14 +5,18 @@ use std::env;
 
 #[test]
 async fn completion() {
-    let client = Client::new(env::args()
-        .nth_back(0)
+    let client = Client::new(env::var("OPENAI_KEY")
         .unwrap()
         .as_str());
 
     let args = Args::new("say this is a test", Option::None, Option::None, Option::None, Option::None);
 
-    let resp = client.create_completion(args).await.unwrap().json::<Response>().await.unwrap();
+    let resp = client.create_completion(args)
+        .await
+        .unwrap()
+        .json::<Response>()
+        .await
+        .unwrap();
 
     assert_eq!(resp.model, "gpt-3.5-turbo".to_string());
 }
