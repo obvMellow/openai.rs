6b940b860758094cf8ff0089dd2f574494ad4e66
obvMellow <darkenedmellow@protonmail.com>
2023-03-02 16:58:15 +0300

    Added Choice and Usage structs

--- src/response.rs
+++ src/response.rs
@@ -1,12 +1,26 @@
-use std::collections::HashMap;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct Response {
     pub id: String,
     pub object: String,
     pub created: i32,
     pub model: String,
-    pub choices: Vec<HashMap<String, String>>,
-    pub usage: HashMap<String, i32>
+    pub choices: Vec<Choice>,
+    pub usage: Usage
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Choice {
+    pub text: String,
+    pub index: i32,
+    pub logprobs: bool,
+    pub finish_reason: String
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Usage {
+    pub prompt_tokens: i32,
+    pub completion_tokens: i32,
+    pub total_tokens: i32,
 }
