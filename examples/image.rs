0fb17145ff879ec29ac5128c3088d6fd1c8905a6
obvMellow <darkenedmellow@protonmail.com>
2023-03-04 13:02:42 +0300

    Added example for image creation API

---
+++ examples/image.rs
@@ -0,0 +1,29 @@
+use openai::{args::{ImageArgs, ImageSize}, response::Content, client::Client};
+use std::io::{stdin, stdout, Write};
+use std::fs;
+
+#[tokio::main]
+async fn main() {
+    let mut prompt = String::new();
+
+    print!("Enter a prompt: ");
+    let _ = stdout().flush();
+
+    stdin()
+        .read_line(&mut prompt)
+        .unwrap();
+
+    println!("Generating image...\n");
+
+    let args = ImageArgs::new(prompt, Some(1), Some(ImageSize::Big), None);
+
+    let client = Client::new(fs::read_to_string("key.txt")
+        .unwrap()
+        .as_str());
+
+    let resp = client.create_image(&args).await.unwrap();
+
+    let url = resp.get_content(0).await.unwrap();
+
+    println!("Url: {}", url);
+}
