adfd0665b1dbbca12398f52289468ca556a10e22
obvMellow <darkenedmellow@protonmail.com>
2023-03-07 19:50:21 +0300

    Added chat completion example

--- examples/completion.rs
+++ examples/completion.rs
@@ -1,6 +1,6 @@
 use openai_rs::{client::Client, args::CompletionArgs, response::Content};
 use std::io::{stdin, stdout, Write};
-use std::fs;
+use std::env;
 
 #[tokio::main]
 async fn main() {
@@ -15,7 +15,7 @@ async fn main() {
 
     let args = CompletionArgs::new(&prompt, None, None, None, None);
 
-    let client = Client::new(fs::read_to_string("key.txt")
+    let client = Client::new(env::var("OPENAI_API_KEY")
         .unwrap()
         .as_str());
 
